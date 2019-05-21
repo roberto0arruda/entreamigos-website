e7d3ef269fdd62056d839df786b002addb40930c
Roberto Arruda <roberto0arruda@hotmail.com>
2019-05-21 11:47:30 -0400

    edit head to secure_asset protocol

--- resources/views/site/includes/head.blade.php
+++ resources/views/site/includes/head.blade.php
@@ -7,7 +7,7 @@
 <title>@yield('title')</title>
 
 <!-- Bootstrap core CSS -->
-<link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
   
 <!-- Custom styles for this template -->
-<link href="{{ asset('css/modern-business.css') }}" rel="stylesheet">
+<link href="{{ secure_asset('css/modern-business.css') }}" rel="stylesheet">
